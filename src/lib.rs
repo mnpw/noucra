374a84e7b9dcbb4c086e68760371a5aa41d54526
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2022-06-16 16:16:55 +0530

    Add skeleton for endpoint to subscribe to a newsletter

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,26 @@ async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(form: web::Form<FormData>) -> impl Responder {
+    let s = {};
+
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -17,8 +17,8 @@ fn spawn_app() -> String {
 #[tokio::test]
 async fn health_check_works() {
     let addr = spawn_app();
-
     let client = reqwest::Client::new();
+
     let response = client
         .get(format!("{}/health_check", addr))
         .send()
@@ -27,4 +27,50 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_for_valid_data() {
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=mrinal%20paliwal&email=dummy%40mail.com";
+    let response = client
+        .post(format!("{}/subscriptions", addr))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Request failed.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_for_invalid_data() {
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=mrinal%20paliwal", "missing email"),
+        ("email=dummy%40mail.com", "missing name"),
+        ("", "missing name and email"),
+    ];
+
+    for (body, case) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", addr))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Request failed.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "Did not fail with payload {}",
+            case
+        );
+    }
 }
