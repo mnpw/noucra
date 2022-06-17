7e257e45ff3b5c4095ff0d454086274fdb628563
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2022-06-18 01:18:10 +0530

    Touch up and run migrate

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -13,12 +13,12 @@ set -eo pipefail
 # [ -x "$(command -v <command>"] translates to
 # test -x "..." which means
 # run "$(...)" and test if it is an executable
-if ! [ -x "$(command -v psql)"]; then
+if ! [ -x "$(command -v psql)" ]; then
     echo >&2 "Error: psql not installed."
     exit 1
 fi
 
-if ! [ -x "$(command -v sqlx)"]; then
+if ! [ -x "$(command -v sqlx)" ]; then
     echo >&2 "Error: sqlx not installed."
     exit 1
 fi
@@ -35,20 +35,22 @@ DB_PORT=${POSTGRES_PORT:=5432}
 # -e: set env variables
 # -p: [host port]:[container port] mapping
 # -N: max connections
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_NAME=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
+if [[-z "${SKIP_DOCKER}"]]; then
+    docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_NAME=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        -d postgres \
+        postgres -N 1000
+fi
 
 # ping postgres and proceed if it is alive and ready
 # export password to be used by `psql`
 export PGPASSWORD="${DB_PASSWORD}"
 # -d "postgres": connect to default supplied db
 # -c "\q": run exit command when connected
-while psql -h "localhost" -p "${DB_PORT}" -U "${DB_USER}" -d "postgres" -c "\q"; do
+until psql -h "localhost" -p "${DB_PORT}" -U "${DB_USER}" -d "postgres" -c "\q"; do
     echo >&2 "Postgres is unavailable - sleeping."
     sleep 1
 done
@@ -60,3 +62,6 @@ echo >&2 "Postgres is running on port ${DB_PORT}."
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
+sqlx migrate run
+
+echo >&2 "Postgres has been migrated."
